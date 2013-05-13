9db187115c1b8e7f6d6d25b1183131b8e51b07f9
xuechong87 <xuechong87@gmail.com>
2013-05-13 22:36:40 +0800

    add ignore

--- src/main/com/xuechong/test/Hello.java
+++ src/main/com/xuechong/test/Hello.java
@@ -1,15 +1,17 @@
 package com.xuechong.test;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class Hello {
-	private static final Map<String,String> cityMap = new HashMap<String, String>();
-	static{
+	private static final Map<String, String> cityMap = new HashMap<String, String>();
+	static {
 		cityMap.put("1", "OneCity");
 		cityMap.put("2", "TwoCity");
 		cityMap.put("3", "ThreeCity");
 	}
-	
-	
+
+
 }
