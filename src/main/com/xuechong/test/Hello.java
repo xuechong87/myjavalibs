c8908c35520734699af282e61cdf0922cfa0dee2
xuechong <xuechong87@gmail.com>
2013-07-26 17:01:37 +0800

    add nio tests

--- src/main/com/xuechong/test/Hello.java
+++ src/main/com/xuechong/test/Hello.java
@@ -2,18 +2,31 @@ package com.xuechong.test;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 
 public class Hello {
+	//13311
+	//40891
+	//59412
+	//64045
 	public static void main(String[] args) {
-		System.out.println(Integer.MAX_VALUE+1 == Integer.MIN_VALUE);
-//		int count =0;
-//		for (int i = 0x7fffffff-100; i <= 0x7fffffff; i++) {
-//			count ++;
-//			System.out.println(count);
-//		}
-//		System.out.println("fin");
+		for (int i = 40891; i < 59412; i++) {
+			System.out.print((char)(i));
+			System.out.println(i);
+		}
+		//\u33ff
+		//\ufa2d
+		
+		System.out.println(Integer.toHexString(13311));
+		System.out.println(Integer.toHexString(40891));
+		System.out.println(Integer.toHexString(59412));
+		System.out.println(Integer.toHexString(64045));
+		System.out.println("\u4cfe");
+		System.out.println("\u4E00");
+		System.out.println("\u9FA5");
+		System.out.println("\u9FA6");
 	}	
 	
 	public static void method1(){
